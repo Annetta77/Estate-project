2249ec17814a8424d076ad8ce9836c39415aa7f0
Anna <ania.ania.1996@mail.ru>
2025-02-23 22:07:46 +0300

    исправления меню

--- src/js/scripts/index.js
+++ src/js/scripts/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const burgerButton = document.getElementById('burgerButton');
   const navMenu = document.getElementById('nav');
+  const navList = document.getElementById('nav-list');
   const navSocial = document.getElementById('nav-social');
   const select = document.getElementById('select');
   const screenWidth = window.innerWidth;
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   burgerButton.addEventListener('click', function () {
     navMenu.classList.toggle('open');
+    navList.classList.toggle('open');
     navSocial.classList.toggle('open');
     burgerButton.classList.toggle('open');
   });
