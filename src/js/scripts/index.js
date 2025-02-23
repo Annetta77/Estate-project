a3d2d82a9967857379731c5ab24867c7b8e5988b
Anna <ania.ania.1996@mail.ru>
2025-02-23 23:27:01 +0300

    mob убран select, добавлен ul

--- src/js/scripts/index.js
+++ src/js/scripts/index.js
@@ -3,20 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const navMenu = document.getElementById('nav');
   const navList = document.getElementById('nav-list');
   const navSocial = document.getElementById('nav-social');
-  const select = document.getElementById('select');
-  const screenWidth = window.innerWidth;
-
-  function updateSelect() {
-    if (screenWidth < 1023) {
-      select.setAttribute('multiple', '');
-    } else {
-      select.removeAttribute('multiple');
-    }
-  };
-
-  updateSelect(select);
-
-  window.addEventListener('resize', updateSelect);
 
   burgerButton.addEventListener('click', function () {
     navMenu.classList.toggle('open');
